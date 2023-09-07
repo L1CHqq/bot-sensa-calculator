9b6e3168f6c56c02c0f162193c1593ca3a3f609d
L1CHqq <142311888+L1CHqq@users.noreply.github.com>
2023-09-07 23:43:25 +0500

    last update

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize') 
 
 module.exports = new Sequelize(
-    'user',
-    'gen_user',
-    'root_root92',
+    '',
+    '',
+    '',
     {
-        host: '92.51.38.28',
-        port: '5432',
+        host: '',
+        port: '',
         dialect: 'postgres'
     }
 )
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramApi = require('node-telegram-bot-api');
-const token = '5813276923:AAHib1X8sW3shPx6kpfOQ_-pS4Z3HVFSsWc';
+const token = 'TOKEN';
 
 const bot = new TelegramApi(token, { polling: true });
 
